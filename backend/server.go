3cdd3651319f76931ffc0799e610afbae0542827
Gift Mugweni <giftmugweni@gmail.com>
2025-01-04 21:11:59 +0200

    added check for existing name in database

--- backend/server.go
+++ backend/server.go
@@ -3,23 +3,63 @@ package main
 import (
 	"ShaderLand/sheets"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+var env map[string]string
+
 func main() {
+	temp, err := godotenv.Read()
+	if err != nil {
+		log.Fatal("Failed to load environment variables")
+	}
+	env = temp
+
 	r := mux.NewRouter()
 	r.HandleFunc("/test", handleTest).Methods("GET")
+	r.HandleFunc("/test", handleTestPost).Methods("POST")
 
 	log.Println("Starting server on: http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func handleTest(w http.ResponseWriter, r *http.Request) {
-	response := sheets.GetSheetData()
-
 	w.Header().Add("Content-Type", "text/json")
+
+	response, err := sheets.GetShaderDetail(env["SPREAD_SHEET_ID"], "test")
+	if err != nil {
+		log.Print(err)
+		http.NotFound(w, r)
+		return
+	}
+
 	json.NewEncoder(w).Encode(response)
+}
+
+func handleTestPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
+	data := sheets.ShaderDetail{}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		response := map[string]string{"message": "Bad Request", "detail": "Request body not correctly structured"}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	err = sheets.AppendShaderDetail(env["SPREAD_SHEET_ID"], data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response := map[string]string{"message": "Internal Server Error", "detail": fmt.Sprint(err)}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
--- backend/sheets/sheets.go
+++ backend/sheets/sheets.go
@@ -2,15 +2,19 @@ package sheets
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
-type Response struct {
-	Name string `json:"name"`
-	Code string `json:"code"`
+type ShaderDetail struct {
+	UserId      string `json:"userId"`
+	Name        string `json:"name"`
+	Tags        string `json:"tags"`
+	Description string `json:"description"`
+	Code        string `json:"code"`
 }
 
 func getSheetsService() *sheets.Service {
@@ -26,22 +30,103 @@ func getSheetsService() *sheets.Service {
 	return service
 }
 
-func GetSheetData() []Response {
+func GetShaderDetail(spreadSheetId string, shaderName string) (ShaderDetail, error) {
+	details := ShaderDetail{}
+	errorMsg := fmt.Errorf("Could not find details for shader: %v", shaderName)
+
 	service := getSheetsService()
-	spreadSheetId := "1kReHyEqnuPh9QTlSt7FeVno9DDn6Qfc0hpbYlzpC6hc"
-	readRange := "Sheet1!A2:E"
+	readRange := "Shaders!A2:E"
 	response, err := service.Spreadsheets.Values.Get(spreadSheetId, readRange).Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+		return details, errorMsg
+	}
+	if len(response.Values) == 0 {
+		return details, errorMsg
 	}
 
-	details := make([]Response, 0)
-	for _, row := range response.Values {
-		var temp Response
-		temp.Name = row[0].(string)
-		temp.Code = row[1].(string)
-		details = append(details, temp)
+	row, err := search(response.Values, shaderName, 1)
+	if err != nil {
+		return details, errorMsg
 	}
 
-	return details
+	details.UserId = row[0].(string)
+	details.Name = row[1].(string)
+	details.Tags = row[2].(string)
+	details.Description = row[3].(string)
+	details.Code = row[4].(string)
+
+	return details, nil
+}
+
+func AppendShaderDetail(spreadSheetId string, details ShaderDetail) error {
+	service := getSheetsService()
+
+	nameExists, errMsg := checkEntryExists(service, spreadSheetId, details.Name, "B2:B", 0)
+	if errMsg != nil {
+		return errMsg
+	}
+	if nameExists {
+		return fmt.Errorf("Name already exists: %s", details.Name)
+	}
+
+	valueRange := sheets.ValueRange{}
+	valueRange.Values = make([][]interface{}, 1)
+	valueRange.Values[0] = make([]interface{}, 5)
+	valueRange.Values[0][0] = details.UserId
+	valueRange.Values[0][1] = details.Name
+	valueRange.Values[0][2] = details.Tags
+	valueRange.Values[0][3] = details.Description
+	valueRange.Values[0][4] = details.Code
+
+	_, err := service.Spreadsheets.Values.Append(spreadSheetId, "A:E", &valueRange).ValueInputOption("RAW").Do()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func checkEntryExists(service *sheets.Service, spreadSheetId string, entry string, sheetRange string, col int) (bool, error) {
+	details, err := service.Spreadsheets.Values.Get(spreadSheetId, sheetRange).Do()
+	if err != nil {
+		return false, err
+	}
+
+	_, err2 := search(details.Values, entry, col)
+	if err2 != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func search(values [][]interface{}, search string, col int) ([]interface{}, error) {
+	startIndex := 0
+	endIndex := len(values) - 1
+
+	for {
+		midIndex := (startIndex + endIndex) / 2
+
+		testValue := values[midIndex][col].(string)
+		if testValue == search {
+			return values[midIndex], nil
+		}
+		if values[startIndex][col].(string) == search {
+			return values[startIndex], nil
+		}
+		if values[endIndex][col].(string) == search {
+			return values[endIndex], nil
+		}
+
+		if startIndex == endIndex || startIndex+1 == endIndex {
+			return make([]interface{}, 0), fmt.Errorf("Could not find: %s", search)
+		}
+
+		if testValue < search {
+			startIndex = midIndex
+		} else {
+			endIndex = midIndex
+		}
+	}
 }
